96c310319c54648caba8c0eb580bf765a19abf61
NĐH <hungcdsphg@gmail.com>
2025-12-17 19:37:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
 import google.generativeai as genai
+
+load_dotenv()
 
 st.sidebar.header("🔐 Test Gemini API")
 
-if st.sidebar.button("Test Gemini API"):
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+if st.sidebar.button("Test Gemini"):
     try:
         model = genai.GenerativeModel("gemini-2.5-flash")
-        response = model.generate_content("Chỉ trả lời: OK")
+        res = model.generate_content("Chỉ trả lời: OK")
         st.sidebar.success("✅ GEMINI API HOẠT ĐỘNG")
-        st.sidebar.code(response.text)
+        st.sidebar.code(res.text)
     except Exception as e:
         st.sidebar.error("❌ LỖI GEMINI API")
         st.sidebar.code(str(e))
